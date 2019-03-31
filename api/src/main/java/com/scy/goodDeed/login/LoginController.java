f14183ccf5076a8056367f391e8997a5dab432fc
totti <253262275@qq.com>
2019-03-31 13:08:57 +0000

    2019-3-31-create-a-new-project

---
+++ api/src/main/java/com/scy/goodDeed/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.scy.goodDeed.login;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "111";
+    }
+}
---
+++ service/src/main/java/com/scy/goodDeed/login/LoginService.java
@@ -0,0 +1,8 @@
+package com.scy.goodDeed.login;
+
+public class LoginService {
+
+    public void login() {
+        System.out.println(111);
+    }
+}
